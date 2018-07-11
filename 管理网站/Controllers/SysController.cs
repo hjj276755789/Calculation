8671a43805c475e8296a72fd016597ac9aca7cdf
losthang <losthang@163.com>
2018-07-12 07:12:31 +0800

    权限

---
+++ 管理网站/Controllers/SysController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 管理网站.Controllers
+{
+    public class SysController : Controller
+    {
+        // GET: Sys
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult yhgl()
+        {
+            return View();
+        }
+        public ActionResult jsgl()
+        {
+            return View();
+        }
+        public ActionResult qxgl()
+        {
+            return View();
+        }
+    }
+}
---
+++ 管理网站/Security/IdentityCheck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 管理网站.Security
+{
+    public class IdentityCheck: AuthorizeAttribute
+    {
+    }
+}
